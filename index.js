dca849f0b1ebe89d9ad9f55664a8527fbbae90e7
Juliette De Meo <gigialiasjuliette@gmail.com>
2025-12-06 10:25:22 +0100

    create User simple model to test connection between front and db

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
+
+const mongoose = require("mongoose");
+const User = require("./models/User");
+
 require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(express.json());
 
 // Route de test
 app.get("/", (req, res) => {
+  console.log("test");
   res.json({ message: "✅ Serveur fonctionne !" });
 });
 
-app.post("/register", (req, res) => {
-  res.send("POST Request Called");
+app.post("/register", async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ message: error.message, errors: error.errors });
+  }
 });
 
 mongoose
---
+++ models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  nickname: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
